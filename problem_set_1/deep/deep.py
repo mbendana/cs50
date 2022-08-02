eab7c24a7981dffb4dd32292a0a0d99b5c6ae47f
mbendana <miltonbendana@outlook.com>
2022-08-02 18:47:56 +0000

    Create deep.py file

---
+++ problem_set_1/deep/deep.py
@@ -0,0 +1,7 @@
+userAnswer = input("What is the Answer to the Great Question of Life, the Universe and Everything? ")
+
+match userAnswer.lower():
+	case "42" | "forty-two" | "forty two":
+		print("Yes")
+	case _:
+		print("No")
