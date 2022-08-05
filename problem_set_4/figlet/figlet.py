522fb55c68fa511277c0187d8747a6a67109a948
mbendana <miltonbendana@outlook.com>
2022-08-05 19:54:25 +0000

    Modify figlet.py file

--- problem_set_4/figlet/figlet.py
+++ problem_set_4/figlet/figlet.py
@@ -3,6 +3,7 @@ from pyfiglet import Figlet
 import random
 
 figlet = Figlet()
+exit_message = f"Invalid usage.\nUsage: {sys.argv[0]} [-f | --font <font_name>]"
 
 if len(sys.argv) == 3:
 	if sys.argv[1] in ["-f", "--font"]:
@@ -10,14 +11,14 @@ if len(sys.argv) == 3:
 		if user_font in figlet.getFonts():
 			figlet.setFont(font=user_font)
 		else:
-			sys.exit(f"Invalid usage.\nUsage: {sys.argv[0]} [-f | --font <font_name>]")
+			sys.exit(exit_message)
 	else:
-		sys.exit(f"Invalid usage.\nUsage: {sys.argv[0]} [-f | --font <font_name>]")
+		sys.exit(exit_message)
 elif len(sys.argv) == 1:
 	user_font = random.choice(figlet.getFonts())
 	figlet.setFont(font=user_font)
 else:
-	sys.exit(f"Invalid usage.\nUsage: {sys.argv[0]} [-f | --font <font_name>]")
+	sys.exit(exit_message)
 
 user_input = input("Input: ")
 print(figlet.renderText(user_input))
